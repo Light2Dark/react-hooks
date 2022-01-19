75786e2cd7a5c0cb70e965535321bc758c6140be
Shahmir Varqha <Sham9871@gmail.com>
2022-01-20 01:25:45 +0800

    useState done, container width shld expand

--- pages/react-hooks.tsx
+++ pages/react-hooks.tsx
@@ -1,4 +1,16 @@
+import { useState } from "react"
+
 export default function Header() {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+
     return(
         <div>
             <h2 className="text-center text-2xl m-2 font-bold">React Hooks</h2>
@@ -9,10 +21,13 @@ export default function Header() {
                 </div>
                 <div className="flex flex-row items-center gap-4">
                     <div className="h-16 w-1/4 bg-red-400 rounded-md flex justify-center items-center">
-                        <p className="font-bold">0</p>
+                        <p className="font-bold">{count}</p>
                     </div>
                     <div>
-                        <h4 className="font-medium">Counter</h4>
+                        <div className="flex flex-row gap-2">
+                            <button onClick={increment} className="font-medium bg-violet-500 py-1 px-5 rounded-lg text-white hover:bg-violet-600">Counter</button>
+                            <button onClick={reset} className="font-medium bg-sky-400 py-1 px-5 rounded-lg text-white hover:bg-sky-500">Reset</button>
+                        </div>
                         <p className="text-slate-500">Click the counter!</p>
                     </div>
 
