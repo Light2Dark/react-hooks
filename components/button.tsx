65c70aa77f5755286f7daa358f5a3080ce32f597
Shahmir Varqha <Sham9871@gmail.com>
2022-01-20 22:08:37 +0800

    made button and card into ui components

--- components/button.tsx
+++ components/button.tsx
@@ -1,13 +1,29 @@
 interface Props {
-    onClick: () => void
+    onClick: () => void;
     buttonText: string;
-    className?: string;
+    colour: "blue" | "purple" | "orange";
+    addClass?: string;
 }
 
-const Button = ({onClick, buttonText, className}: Props) => {
+const Button = ({onClick, buttonText, colour, addClass}: Props) => {
+    let colourClass
+    switch (colour) {
+        case "blue":
+            colourClass = "bg-sky-400 hover:bg-sky-500"
+            break
+        case "purple":
+            colourClass = "bg-violet-500 hover:bg-violet-600"
+            break
+        case "orange":
+            colourClass = "bg-orange-400 hover:bg-orange-500"
+            break
+        default: 
+            break;
+    }
+
     return(
         <>
-            <button onClick={onClick} className={`font-medium py-1 px-5 rounded-lg text-white ${className}`} >{buttonText}</button>
+            <button onClick={onClick} className={`font-medium py-1 px-5 rounded-lg text-white ${colourClass} ${addClass}`} >{buttonText}</button>
         </>
     )
 }
--- pages/react-hooks.tsx
+++ pages/react-hooks.tsx
@@ -1,31 +1,57 @@
-import { useState, useReducer } from "react"
+import { useState} from "react"
 import Button from "../components/button"
+import Reducer from "./hooks/useReducer"
 
-export default function Header() {
+interface Props {
+    content: JSX.Element;
+    name: string;
+    divClass?: string;
+}
+
+const Card = ({name, content, divClass}: Props) => {
+    return(
+        <>
+            <div className={`bg-white mx-auto p-4 rounded flex flex-col shadow-md mb-4 ${divClass}`}> 
+                <div className="mb-1">
+                    <h3 className="font-bold text-center">{name}</h3>
+                </div>
+                {content}
+            </div>
+        </>
+    )
+}
+
+const StateHook = () => {
     const [count, setCount] = useState(0)
+    // const [test, setTest] = useState<number | undefined>(undefined);
+    // const [value, setValue] = useState<Array<number>>([]);
 
     return(
-        <div>
-            <h2 className="text-center text-2xl m-2 font-bold">React Hooks</h2>
-
-            <div className="bg-white max-w-sm mx-auto p-4 rounded flex flex-col shadow-md"> 
-                <div className="mb-1">
-                    <h3 className="font-bold text-center">useState</h3>
-                </div>
-                <div className="flex flex-row items-center gap-4">
+        <>
+            <div className="flex flex-row items-center gap-4">
                     <div className="h-16 w-1/4 bg-red-400 rounded-md flex justify-center items-center">
                         <p className="font-bold">{count}</p>
                     </div>
                     <div>
                         <div className="flex flex-row gap-2">
-                            <Button onClick = {() => setCount(count + 1)} buttonText="Counter" className="bg-violet-500 hover:bg-violet-600"/>
-                            <Button onClick={() => setCount(0)} className="bg-sky-400 hover:bg-sky-500" buttonText="Reset"/>
+                            <Button onClick = {() => setCount(count + 1)} buttonText="Counter" colour="purple"/>
+                            <Button onClick={() => setCount(0)} colour="blue" buttonText="Reset"/>
                         </div>
                         <p className="text-slate-500">Click the counter!</p>
                     </div>
-
-                </div>
             </div>
-        </div>
+        </>
+    )
+}
+
+export default function Header() {
+
+    return(
+        <>
+            <h2 className="text-center text-2xl m-2 font-bold">React Hooks</h2>
+
+            <Card content={<StateHook />} name="useState" divClass="max-w-sm" />
+            <Card content={<Reducer />} name="useReducer" divClass="max-w-md" />
+        </>
     )
 }
